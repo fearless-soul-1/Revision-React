308014aab6d669ae345e8b89204fa54dc0fdf928
fearless-soul-1 <shikharkatiyar558@gmail.com>
2025-07-24 23:25:39 +0530

    Day-1.4

---
+++ javaScript-inside-jsx/src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+import './ExpenseItem.css';
+function ExpenseItem() {
+  const expenseDate = new Date().toISOString();
+  const expenseTitle = "Insurance";
+  const expenseAmount = 50;
+  const expenseLocation = "Bangalore";
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__location">
+      {expenseLocation}
+      </div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+  
+}
+
+export default ExpenseItem;
