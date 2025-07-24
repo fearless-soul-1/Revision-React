6f3bcfc490e57b6bd02bf0c871207ce350edb429
fearless-soul-1 <shikharkatiyar558@gmail.com>
2025-07-24 22:46:33 +0530

    Day-1.2

---
+++ writing-complex-jsx/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>15 August 2023</div>
+      <div>Delhi</div>
+      <div>
+        <h2>Book</h2>
+        <div>$10</div>
+      </div>
+    </>
+  );
+}
+
+export default ExpenseItem;
