8e2cdf2ec01599e74f57a02b6f2b8dc74c0951d0
fearless-soul-1 <shikharkatiyar558@gmail.com>
2025-07-26 00:02:34 +0530

    Day-1.7

---
+++ formatting-date-calendar-look/src/App.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  const expenses = [
+    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 , location:"Bangalore"},
+    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10, location:"Delhi" },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1, location:"Hyderabad" },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200, location:"Mumbai" },
+  ];
+
+  return (
+    <div>
+      <h1>Let's get Started</h1>
+      {expenses.map((expense)=>{
+        return (<ExpenseItem key={expense.id} date={expense.date} title={expense.title} location={expense.location} price={expense.price} />)
+      })}
+      
+    </div>
+  );
+}
+
+export default App;
