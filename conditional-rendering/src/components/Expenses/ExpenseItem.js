69a191ee42ce9f602c72a27dcbd64c98ab5e05d1
fearless-soul-1 <shikharkatiyar558@gmail.com>
2025-07-29 22:33:34 +0530

    Day-2.10

---
+++ conditional-rendering/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate.js";
+import Card from "../../UI/Card.js";
+
+function ExpenseItem(props) {
+
+  return (
+    <li>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__location">{props.location}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
+    </Card>
+    </li>
+  );
+}
+
+export default ExpenseItem;
